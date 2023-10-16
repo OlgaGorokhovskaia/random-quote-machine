14b2644e2468eaaa967507c7f6ae052bd256ed0c
Olga Gorokhovskaia <olga.gorokhovskaia@gmail.com>
2023-10-16 16:24:20 -0400

    add screenshot

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
-import reportWebVitals from './reportWebVitals';
 import AppWithState from './App';
 import './index.css';
 
@@ -21,8 +20,3 @@ class Root extends React.Component {
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<Root />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
